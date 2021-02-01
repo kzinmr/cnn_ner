026ebe17e3d73c9df63c3e3c848dfefe4e76f37d
Kazuki Inamura <kzinmr109@gmail.com>
2021-02-01 12:27:38 +0900

    fix: to use cpu in prediction

--- main.py
+++ main.py
@@ -1155,111 +1155,6 @@ class TokenClassificationModel(nn.Module):
         return scores, tag_seq
 
 
-class _Alphabet:
-    def __init__(self, name, keep_growing=False):
-        self.name = name
-        self.UNKNOWN = "</unk>"
-        self.instance2index = {}
-        self.instances = []
-        self.keep_growing = keep_growing
-
-        # Index 0 is occupied by default, all else following.
-        self._default_index = 0
-        self._next_index = 1
-        self.add(self.UNKNOWN)
-
-    def clear(self):
-        self.instance2index = {}
-        self.instances = []
-
-        # Index 0 is occupied by default, all else following.
-        self._default_index = 0
-        self._next_index = 1
-
-    def add(self, instance):
-        if instance not in self.instance2index:
-            self.instances.append(instance)
-            self.instance2index[instance] = self._next_index
-            self._next_index += 1
-
-    def get_index(self, instance):
-        if instance in self.instance2index:
-            return self.instance2index[instance]
-        else:
-            if self.keep_growing:
-                index = self._next_index
-                self.add(instance)
-                return index
-            else:
-                return self.instance2index[self.UNKNOWN]
-
-    def get_instance(self, index):
-        try:
-            return self.instances[index - 1]
-        except IndexError:
-            print(
-                "WARNING:Alphabet get_instance ,unknown instance, return the first label."
-            )
-            return self.instances[0]
-
-    def size(self):
-        return len(self.instances) + 1
-
-    def items(self):
-        return self.instance2index.items()
-
-    def close(self):
-        self.keep_growing = False
-
-    def open(self):
-        self.keep_growing = True
-
-    def get_content(self):
-        return {"instance2index": self.instance2index, "instances": self.instances}
-
-    def from_json(self, data):
-        self.instances = data["instances"]
-        self.instance2index = data["instance2index"]
-
-    def save(self, output_directory, name=None):
-        """
-        Save both alhpabet records to the given directory.
-        :param output_directory: Directory to save model and weights.
-        :param name: The alphabet saving name, optional.
-        :return:
-        """
-        saving_name = name if name else self.name
-        try:
-            json.dump(
-                self.get_content(),
-                open(os.path.join(output_directory, saving_name + ".json"), "w"),
-            )
-        except Exception as e:
-            print("Exception: Alphabet is not saved: " % repr(e))
-
-    def load(self, input_directory, name=None):
-        """
-        Load model architecture and weights from the give directory. This allow we use old models even the structure
-        changes.
-        :param input_directory: Directory to save model and weights
-        :return:
-        """
-        loading_name = name if name else self.name
-        self.from_json(
-            json.load(open(os.path.join(input_directory, loading_name + ".json")))
-        )
-
-    @staticmethod
-    def normalize_word(word):
-        new_word = ""
-        for char in word:
-            if char.isdigit():
-                new_word += "0"
-            else:
-                new_word += char
-        return new_word
-
-
 class Alphabet:
     def __init__(self, name, label=False, keep_growing=False):
         self.name = name
@@ -2086,12 +1981,13 @@ class TokenClassificationModule(pl.LightningModule):
 
         elif hparams.do_predict:
             self.model_path = hparams.model_path
-            if self.gpu:
-                loaded_state = torch.load(self.model_path)
-            else:
-                loaded_state = torch.load(
-                    self.model_path, map_location=torch.device("cpu")
-                )
+            # if self.gpu:
+            #     loaded_state = torch.load(self.model_path)
+            # else:
+            self.gpu = False
+            loaded_state = torch.load(
+                self.model_path, map_location=torch.device("cpu")
+            )
             params = self._load_params(self.config_path)
             print(f"Loaded params from {self.config_path}: {params}")
             assert (
