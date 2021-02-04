e2c2f4b9e74b65b5281d0307733ca4d651311b97
Kazuki Inamura <kzinmr109@gmail.com>
2021-02-04 18:14:14 +0900

    fix

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 python3 main.py \
---data_dir="/app/workspace/data/" \
+--data_dir="/app/workspace/conll03/" \
 --train_batch_size=128 \
 --eval_batch_size=64 \
 --num_workers=6 \
---pretrain_embed_path="/app/workspace/models/word2vec.txt" \
 --learning_rate=0.015 \
 --anneal_factor=0.5 \
 --dropout=0.2 \
@@ -12,8 +11,9 @@ python3 main.py \
 --output_dir="/app/workspace/output/" \
 --config_path="/app/workspace/models/train.config" \
 --vocab_path="/app/workspace/models/" \
---model_path="/app/workspace/models/checkpoint-epoch=40-val_f1=0.60.ckpt" \
 --do_train \
 --gpus=1
 # --download
 # --do_predict
+#--pretrain_embed_path="/app/workspace/models/word2vec.txt" \
+#--model_path="/app/workspace/models/checkpoint-epoch=40-val_f1=0.60.ckpt" \
